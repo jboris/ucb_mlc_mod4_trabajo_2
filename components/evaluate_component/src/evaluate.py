ce9a697e60428304f80e2608561a91e6120cb80c
Boris Bellido <jboris.bsm@gmail.com>
2023-11-05 17:50:13 -0400

    Fix output word.

--- components/evaluate_component/src/evaluate.py
+++ components/evaluate_component/src/evaluate.py
@@ -9,7 +9,7 @@ parser.add_argument("--test_data", type=str, help="Test data path")
 parser.add_argument("--predict_data", type=str, help="Predict data path")
 parser.add_argument("--objective", type=str, help="Objective")
 parser.add_argument("--target_names", type=str, help="Target names (separated by commas)")
-parser.add_argument("--report_ouptput", type=str, help="Report data path")
+parser.add_argument("--report_output", type=str, help="Report data path")
 
 args = parser.parse_args()
 
@@ -18,7 +18,7 @@ lines = [
     f"Predict data path: {args.predict_data}",
     f"Objective: {args.objective}",
     f"Target names (separated by commas): {args.target_names}",
-    f"Report data path: {args.report_ouptput}",
+    f"Report data path: {args.report_output}",
 ]
 
 print("Parametros: ...")
@@ -32,5 +32,5 @@ test = pd.read_csv(args.test_data)[args.objective]
 predict = pd.read_csv(args.predict_data)
 report = classification_report(test, predict, target_names=args.target_names.split(','))
 df_report = pd.read_csv(StringIO(report), sep="\\s+")
-df_report.to_csv(args.report_ouptput)
+df_report.to_csv(args.report_output)
 
